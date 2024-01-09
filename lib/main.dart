bfaa2453cdc0fa18164ba1ebec51939b4ff3ceb2
XxThunderBlastxX <koustavmondal55@gmail.com>
2024-01-09 20:59:01 +0530

    Changed to hash url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:confess/app/app.dart';
 import 'package:confess/bootstrap.dart';
 import 'package:confess/helper/firebase_helper.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
-  setPathUrlStrategy();
   await FirebaseHelper.instance.init();
   await bootstrap(() => const App());
 }
